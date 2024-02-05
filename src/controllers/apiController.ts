3585f061469e45d85f0b1b7195151bc19f9cbfd9
RXGabriel <gabriel2005rf@gmail.com>
2024-02-05 15:00:55 -0300

    Implementing the service on the controller

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,53 +1,48 @@
-import { Request, Response } from 'express';
-import { User } from '../models/User';
+import { Request, Response } from "express";
+import * as UserService from "../service/UserService";
 
 export const ping = (req: Request, res: Response) => {
-    res.json({pong: true});
-}
+  res.json({ pong: true });
+};
 
 export const register = async (req: Request, res: Response) => {
-    if(req.body.email && req.body.password) {
-        let { email, password } = req.body;
+  if (req.body.email && req.body.password) {
+    let { email, password } = req.body;
 
-        let hasUser = await User.findOne({where: { email }});
-        if(!hasUser) {
-            let newUser = await User.create({ email, password });
+    const newUser = await UserService.createUser(email, password);
 
-            res.status(201);
-            res.json({ id: newUser.id });
-        } else {
-            res.json({ error: 'E-mail já existe.' });
-        }
+    if (newUser instanceof Error) {
+      res.status(400).json({ error: newUser.message });
+    } else {
+      res.status(201).json({ id: (newUser as any)?.id });
     }
-
-    res.json({ error: 'E-mail e/ou senha não enviados.' });
-}
+  } else {
+    res.status(400).json({ error: "E-mail e/ou senha não enviados." });
+  }
+};
 
 export const login = async (req: Request, res: Response) => {
-    if(req.body.email && req.body.password) {
-        let email: string = req.body.email;
-        let password: string = req.body.password;
+  if (req.body.email && req.body.password) {
+    let email: string = req.body.email;
+    let password: string = req.body.password;
 
-        let user = await User.findOne({ 
-            where: { email, password }
-        });
+    const user = await UserService.findByEmail(email);
 
-        if(user) {
-            res.json({ status: true });
-            return;
-        }
+    if (user && (await UserService.matchPassword(password, user.password))) {
+      res.json({ status: true });
+      return;
     }
-
-    res.json({ status: false });
-}
+  }
+  res.json({ status: false });
+};
 
 export const list = async (req: Request, res: Response) => {
-    let users = await User.findAll();
-    let list: string[] = [];
+  let users = await UserService.listAll();
+  let list: string[] = [];
 
-    for(let i in users) {
-        list.push( users[i].email );
-    }
+  for (let i in users) {
+    list.push(users[i].email);
+  }
 
-    res.json({ list });
-}
+  res.json({ list });
+};
