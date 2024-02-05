2f782f18fef65c7948951ec6d27cc810a2d11f67
RXGabriel <gabriel2005rf@gmail.com>
2024-02-05 15:15:03 -0300

    Preparing environment for database testing

--- src/data/mysql.ts
+++ src/data/mysql.ts
@@ -1,14 +1,10 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import db from "./database";
 
 dotenv.config();
 
-export const sequelize = new Sequelize(
-  process.env.MYSQL_DB as string,
-  process.env.MYSQL_USER as string,
-  process.env.MYSQL_PASSWORD as string,
-  {
-    dialect: "mysql",
-    port: parseInt(process.env.MYSQL_PORT as string),
-  }
-);
+export const sequelize = new Sequelize(db.db, db.user, db.password, {
+  dialect: "mysql",
+  port: parseInt(db.port),
+});
